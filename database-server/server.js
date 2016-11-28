fa2a483ecb7feb9b4bac3a5dc75002e1b8bfa9d3
Dagmara <daga.sz@hotmail.com>
2016-11-28 15:14:47 +0000

    Returning the value

--- database-server/server.js
+++ database-server/server.js
@@ -12,6 +12,10 @@ server.get('/set', function (req, res) {
   res.send('You set: ' + JSON.stringify(storage.params));
 });
 
+server.get('/get', function (req, res){
+  res.send('The value of ' + req.query['key'] + ' is: ' + storage.params[req.query['key']]);
+});
+
 server.listen(4000, function () {
   console.log('Listening on port 4000');
 });
--- database-server/test/returningSpec.js
+++ database-server/test/returningSpec.js
@@ -8,4 +8,12 @@ var storage = require("../storage").Storage;
 
 describe('returning somekey with somevalue', function(){
 
+  it('returns the value stored at somekey', function(done){
+    storage.params = {'somekey': 'donkey'}
+    request.get('http://localhost:4000/get?key=somekey', function (err, res, body){
+      expect(res.body).to.include('donkey');
+      done();
+    });
+  });
+
 });
